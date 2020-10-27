0e269c32ef8bf2fb18ea8f2ca1d03342ad13c871
Flor Marshall <flordmarshall@gmail.com>
2020-10-26 21:59:32 -0400

    Update calculator.js
    Calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,32 @@
-function add () {
-	
+function add(a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract(a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum(a, b) {
+	return Array.reduce((total, current) => total + current, 0);
 }
 
-function multiply () {
-	
+function multiply(a, b) {
+	return Array.length
+		? Array.reduce((accumulator, nextItem) => accumulator * nextItem)
+		: 0;
 }
 
-function power() {
-	
+function power(a, b) {
+	return Math.pow(a, b)
 }
 
-function factorial() {
-	
+function factorial(n) {
+	if (n == 0) return 1;
+	let example = 1;
+	for (let i = n; i > 0; i--) {
+		example *= i;
+	}
+	return example
 }
 
 module.exports = {
@@ -27,6 +34,6 @@ module.exports = {
 	subtract,
 	sum,
 	multiply,
-    power,
+	power,
 	factorial
 }
